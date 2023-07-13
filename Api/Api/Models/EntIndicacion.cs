7d92c538a2cc45f7a0d683ca9a8e79ab10813f26
Andres Arias <ariasandrescr2000@gmail.com>
2023-07-12 21:02:49 -0600

    Se crean el modelo de las tablas y la conexión de la base datos

---
+++ Api/Api/Models/EntIndicacion.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Models
+{
+    [Table("indicacion", Schema = "web")]
+    public class EntIndicacion
+    {
+        [Key]
+        public int id_indicacion { get; set; }
+        [Required(ErrorMessage = "Se requiere el usaurio.")]
+        public int id_usuario { get; set; }
+        [Required(ErrorMessage = "Se requiere el nombre.")]
+        public string nombre { get; set; }
+        [Required(ErrorMessage = "Se requiere el tipo.")]
+        public char tipo { get; set; }
+        public byte imagen { get; set; }
+        public string valor { get; set; }
+        public string[] etiqueta { get; set; }
+    }
+}
